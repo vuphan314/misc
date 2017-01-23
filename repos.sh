0b9fb3a33222547051f78664b6dadcf847b1931f
Vu Phan <vuphan314@gmail.com>
2017-01-23 07:02:31 -0600

    clear echo

--- repos.sh
+++ repos.sh
@@ -1,6 +1,8 @@
 function loop {
+  clear
   cd ..
   for d in $(find -mindepth 1 -maxdepth 1); do
+    echo $d
     cd $d
     git status
     echo ===================================================
