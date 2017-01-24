d8455d6a9817dd05c53ce579450f7586e6c19ccc
Vu Phan <vuphan314@gmail.com>
2017-01-24 00:08:32 -0600

    doing histogram

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -1,1 +1,19 @@
-  
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        n = len(heights)
+        if n == 0:
+            return 0
+        if n == 1:
+            return heights[0]
+        if n == 2:
+            return max(heights[0], heights[1], 2 * min(heights))
+        else:
+            return max(
+                self.largestRectangleArea(heights[:-1]),
+                heights[-1],
+!
+            )
