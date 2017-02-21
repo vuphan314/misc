49066a8b3968003749025a0246f7ae2b463cf3b1
Vu Phan <vuphan314@gmail.com>
2017-02-20 19:26:26 -0600

    my correct c++ quicksort

--- tmp/tmp.cc
+++ tmp/tmp.cc
@@ -1,4 +1,49 @@
-int main(int argc, char const *argv[]) {
+using namespace std;
 
-  return 0;
+#include <iostream>
+#include <vector>
+
+template<typename T>
+void swapIndicies(vector<T> &A, size_t i, size_t j) {
+  T tmp = A[i];
+  A[i] = A[j];
+  A[j] = tmp;
+}
+
+template<typename T>
+void swapElement(vector<T> &A, size_t i, T &el) {
+  T tmp = A[i];
+  A[i] = el;
+  el = tmp;
+}
+
+template<typename T>
+T getPivot(vector<T> &A, size_t low, size_t high) {
+  T pivot = A[low];
+  size_t left = low;
+  for (size_t i = low + 1; i <= high; i++) {
+    if (A[i] < pivot) {
+      swapIndicies(A, i, left);
+      left++;
+    }
+  }
+  swapElement(A, left, pivot);
+  return left;
+}
+
+template<typename T>
+void sortquick(vector<T> &A, size_t low, size_t high) {
+  if (low < high) {
+    size_t pivot = getPivot(A, low, high);
+    sortquick(A, low, pivot);
+    sortquick(A, pivot + 1, high);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  vector<int> A{6, 4, 4, 5, 3, -2, -49, 184, -38};
+  sortquick(A, 0, A.size() - 1);
+  for (auto el : A) {
+    cout << el << ", ";
+  }
 }
