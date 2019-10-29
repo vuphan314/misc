91a5240133d3116ae9d29ace0e09de4274b0cf83
Vu Phan <vuphan314@users.noreply.github.com>
2019-10-29 00:04:13 -0500

    tree -L 2 --dirsfirst -I 'Arduino|bin|ViberDownloads|Music|Public|snap|Templates|Zotero'

--- misc.sh
+++ misc.sh
@@ -23,7 +23,7 @@ function set_action {
 function execute_ls {
   cd
   rm -f GlobalModel.txt
-  tree -L 2 --dirsfirst -I 'bin|ViberDownloads|Music|Public|snap|Templates|Zotero'
+  tree -L 2 --dirsfirst -I 'Arduino|bin|ViberDownloads|Music|Public|snap|Templates|Zotero'
 }
 
 ################################################################################
