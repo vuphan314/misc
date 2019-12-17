8f9b53b132502f0cba864a691e435ff977e41dd5
Vu Phan <vuphan314@users.noreply.github.com>
2019-12-17 17:39:43 -0600

    554 final

--- tmp/tmp.py
+++ tmp/tmp.py
@@ -2,12 +2,23 @@
 
 from fractions import *
 from itertools import *
+from math import *
 
 ################################################################################
+
+KiB = 2**10
+MiB = 2**20
+GiB = 2**30
+
+def lg(x):
+    return log(x, 2)
 
 def powerset(iterable): # [1,2,3] |-> [(), (0,), (1,), (2,), (0, 1), (0, 2), (1, 2), (0, 1, 2)]
     s = list(iterable)
     return list(chain.from_iterable(combinations(s, r) for r in range(len(s) + 1)))
+
+def addSeparators(string, separator=' ', spacing=4):
+    return separator.join(string[i : i + spacing] for i in range(0, len(string), spacing))
 
 ################################################################################
 '''COMP 554 midterm'''
@@ -52,12 +63,12 @@ def getBase10FloatFromBase2FractionalPartDecStr(base2FractionalPartDecStr): # '.
     base10Float = 0
     for i in range(len(base2FractionalPartDecStr)):
         if base2FractionalPartDecStr[i] == '1': # effectively skips leading '.'
-            base10Float += 2 ** -i
+            base10Float += 2**-i
     return base10Float
 def getBase10FloatFromBase2DecStr(base2DecStr): # '+1.1' |-> 1.5
     assert base2DecStr[0] in {'+', '-'}
 
-    sign = (-1) ** (base2DecStr[0] == '-')
+    sign = (-1)**(base2DecStr[0] == '-')
     base2DecStr = base2DecStr[1:]
 
     decPointIndex = base2DecStr.index('.')
@@ -77,7 +88,7 @@ def getBase10FloatFromBase2IeeeStr(base2IeeeStr): # '001111111100000000000000000
     exponent = base2IeeeStr[1:9]
     mantissa = base2IeeeStr[9:]
 
-    signFactor = (-1) ** (sign == '1')
+    signFactor = (-1)**(sign == '1')
     exponentFactor = 2**(int(exponent, base=2) - 127)
     mantissaFactor = getBase10FloatFromBase2MantissaStr(mantissa)
 
@@ -221,6 +232,446 @@ def midterm():
     question22()
 
 ################################################################################
+'''COMP 554 final'''
+
+def getTime(hitRate, hitTime, missTime):
+    return hitRate * hitTime + (1 - hitRate) * missTime
+
+def answer_02():
+    hitTime = 1
+    missTime = 10
+    for hitRate in [.97, .99]:
+        time = getTime(hitRate, hitTime, missTime)
+        print(time)
+
+def answer_03():
+    hitTime = 1
+    missTime = 50
+    for hitRate in [.97, .99]:
+        time = getTime(hitRate, hitTime, missTime)
+        print(time)
+
+def answer_04_p1():
+    mainSize = 128 * KiB
+    cacheSize = 16 * KiB
+    blockSize = 256
+
+    sets = cacheSize / blockSize
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = lg(mainSize)
+
+    tagBits = mainBits - blockBits - setBits
+    print(tagBits)
+
+def answer_04_p2():
+    mainSize = 32 * GiB
+    cacheSize = 32 * KiB
+    blockSize = KiB
+
+    sets = cacheSize / blockSize
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = lg(mainSize)
+
+    tagBits = mainBits - blockBits - setBits
+    print(tagBits)
+
+def answer_04_p3():
+    cacheSize = 512 * KiB
+    blockSize = KiB
+    tagBits = 7
+
+    sets = cacheSize / blockSize
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = tagBits + setBits + blockBits
+    mainSize = 2**mainBits
+    mainSize /= MiB
+    print(mainSize, 'MiB')
+
+def answer_04_p4():
+    mainSize = 16 * GiB
+    blockSize = 4 * KiB
+    tagBits = 10
+
+    mainBits = lg(mainSize)
+
+    blockBits = lg(blockSize)
+
+    setBits = mainBits - tagBits - blockBits
+    sets = 2**setBits
+
+    cacheSize = sets * blockSize
+    cacheSize /= MiB
+    print(cacheSize, 'MiB')
+
+def answer_04_p5():
+    mainSize = 64 * MiB
+    tagBits = 10
+
+    mainBits = lg(mainSize)
+
+    setAndBlockBits = mainBits - tagBits
+
+    cacheSize = 2**setAndBlockBits
+    cacheSize /= KiB
+    print(cacheSize, 'KiB')
+
+def answer_04_p6():
+    cacheSize = 512 * KiB
+    tagBits = 7
+
+    setAndBlockBits = lg(cacheSize)
+
+    mainBits = tagBits + setAndBlockBits
+    mainSize = 2**mainBits
+    mainSize /= MiB
+    print(mainSize, 'MiB')
+
+def getSets(cacheSize, blockSize, blocksPerSet):
+    return cacheSize / blockSize / blocksPerSet
+
+def getTagBits(mainBits, sets, blockSize):
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    tagBits = mainBits - setBits - blockBits
+    return tagBits
+
+def getTotalTagBits(cacheSize, blockSize, tagBits):
+    blocks = cacheSize / blockSize
+
+    totalTagBits = blocks * tagBits
+    return totalTagBits
+
+def answer_06():
+    mainBits = 42
+
+    cacheSize1 = 128 * KiB
+    blockSize1 = 64
+    blocksPerSet1 = 1
+
+    cacheSize2 = MiB
+    blockSize2 = 512
+    blocksPerSet2 = 8
+
+    sets1 = getSets(cacheSize1, blockSize1, blocksPerSet1)
+    sets2 = getSets(cacheSize2, blockSize2, blocksPerSet2)
+
+    tagBits1 = getTagBits(mainBits, sets1, blockSize1)
+    tagBits2 = getTagBits(mainBits, sets2, blockSize2)
+
+    def answer1():
+        print(sets1)
+        print(sets2)
+
+    def answer2():
+        print(tagBits1)
+        print(tagBits2)
+
+    def answer3():
+        totalTagBits1 = getTotalTagBits(cacheSize1, blockSize1, tagBits1)
+        totalTagBits2 = getTotalTagBits(cacheSize2, blockSize2, tagBits2)
+
+        print(totalTagBits1)
+        print(totalTagBits2)
+
+    # answer1()
+    # answer2()
+    answer3()
+
+def getCyclesPerInstruction(instructions, exeTime, cycleTime):
+    instructionTime = exeTime / instructions
+    cyclesPerInstruction = instructionTime / cycleTime
+    return cyclesPerInstruction
+
+def getSpeedup(cycleTime, instructionsNew, cyclesPerInstructionNew, exeTimeOld):
+    exeTimeNew = cyclesPerInstructionNew * instructionsNew * cycleTime
+    speedup = exeTimeOld / exeTimeNew
+    return speedup
+
+def answers_10_14(): # Note: give answers to three decimal places, for example: 88.123.
+    cycleTime = 1e-9
+
+    instructionsA = 1e9
+    exeTimeA = 1.1
+
+    instructionsB = 1.2e9
+    exeTimeB = 1.5
+
+    cyclesPerInstructionA = getCyclesPerInstruction(instructionsA, exeTimeA, cycleTime)
+    cyclesPerInstructionB = getCyclesPerInstruction(instructionsB, exeTimeB, cycleTime)
+
+    instructionsNew = 6e8
+    cyclesPerInstructionNew = 1.1
+
+    def answer10():
+        print(cyclesPerInstructionA)
+
+    def answer11():
+        print(cyclesPerInstructionB)
+
+    def answer12():
+        clockRateAOverClockRateB = cyclesPerInstructionB * instructionsB / cyclesPerInstructionA / instructionsA
+        print(clockRateAOverClockRateB)
+
+    def answer13():
+        speedupA = getSpeedup(cycleTime, instructionsNew, cyclesPerInstructionNew, exeTimeA)
+        print(speedupA)
+
+    def answer14():
+        speedupB = getSpeedup(cycleTime, instructionsNew, cyclesPerInstructionNew, exeTimeB)
+        print(speedupB)
+
+    # answer10()
+    # answer11()
+    # answer12()
+    # answer13()
+    answer14()
+
+def answer_15():
+    opcode = bin(0x33)[2:].rjust(7, '0')
+    funct3 = bin(0x0)[2:].rjust(3, '0')
+    funct7 = bin(0x20)[2:].rjust(7, '0')
+    rs2 = bin(5)[2:].rjust(5, '0')
+    rs1 = bin(7)[2:].rjust(5, '0')
+    rd = bin(6)[2:].rjust(5, '0')
+
+    instruction = funct7 + rs2 + rs1 + funct3 + rd + opcode
+    instruction = addSeparators(instruction)
+    print(instruction)
+
+def answer_17():
+    base2DecStr = getBase2DecStrFromBase10DecStr('+63.25')
+    # print(base2DecStr) # 111111.01 == 1.1111101 * 2**5
+
+    signBit = '0'
+
+    bias = 127
+    exponentBits = bin(bias + 5)[2:].rjust(8, '0')
+
+    fractionBits = '1111101'.ljust(23, '0')
+
+    ieeeBits = signBit + exponentBits + fractionBits
+    ieeeBits = addSeparators(ieeeBits)
+    print(ieeeBits) # 0100 0010 0111 1101 0000 0000 0000 0000
+
+def answer_25_p1():
+    mainSize = 128 * KiB
+    cacheSize = 16 * KiB
+    blockSize = 256
+    blocksPerSet = 2
+
+    blocks = cacheSize / blockSize
+    sets = blocks / blocksPerSet
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = lg(mainSize)
+
+    tagBits = mainBits - setBits - blockBits
+    print(tagBits)
+
+def answer_25_p2():
+    mainSize = 32 * GiB
+    cacheSize = 32 * KiB
+    blockSize = KiB
+    blocksPerSet = 4
+
+    blocks = cacheSize / blockSize
+    sets = blocks / blocksPerSet
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = lg(mainSize)
+
+    tagBits = mainBits - setBits - blockBits
+    print(tagBits)
+
+def answer_25_p3():
+    cacheSize = 512 * KiB
+    blockSize = KiB
+    tagBits = 7
+    blocksPerSet = 8
+
+    blocks = cacheSize / blockSize
+    sets = blocks / blocksPerSet
+    setBits = lg(sets)
+
+    blockBits = lg(blockSize)
+
+    mainBits = tagBits + setBits + blockBits
+    mainSize = 2**mainBits
+    mainSize /= MiB
+    print(mainSize, 'MiB')
+
+def answer_25_p4():
+    mainSize = 16 * GiB
+    # blockSize = 4 * KiB
+    tagBits = 10
+    blocksPerSet = 4
+
+    mainBits = lg(mainSize)
+
+    setAndBlockBits = mainBits - tagBits
+
+    cacheSizePerWay = 2**setAndBlockBits
+
+    cacheSize = cacheSizePerWay * blocksPerSet
+    cacheSize /= MiB
+    print(cacheSize, 'MiB')
+
+def answer_25_p5():
+    mainSize = 64 * MiB
+    tagBits = 10
+    blocksPerSet = 4
+
+    mainBits = lg(mainSize)
+
+    setAndBlockBits = mainBits - tagBits
+
+    cacheSizePerWay = 2**setAndBlockBits
+
+    cacheSize = cacheSizePerWay * blocksPerSet
+    cacheSize /= KiB
+    print(cacheSize, 'KiB')
+
+def answer_25_p6():
+    cacheSize = 512 * KiB
+    tagBits = 7
+    blocksPerSet = 8
+
+    cacheSizePerWay = cacheSize / blocksPerSet
+    setAndBlockBits = lg(cacheSizePerWay)
+
+    mainBits = tagBits + setAndBlockBits
+
+    mainSize = 2**mainBits
+    mainSize /= MiB
+    print(mainSize, 'MiB')
+
+def getDieArea(diameter, dies):
+    waferArea = pi * diameter**2 / 4
+    dieArea = waferArea / dies
+    return dieArea
+
+def getWaferYield(diameter, dies, defectRate):
+    dieArea = getDieArea(diameter, dies)
+    waferYield = (1 + defectRate * dieArea / 2)**-2
+    return waferYield
+
+def getDieCost(waferCost, dies, waferYield):
+    dieCost = waferCost / dies / waferYield
+    return dieCost
+
+def getDefectRate(waferYield): # cm^-2
+    dieArea = 200 # mm^2
+    dieArea /= 100 # cm^2
+    defectRate = 2 * (waferYield**-.5 - 1) / dieArea
+    return defectRate
+
+def answers_28_37(): # Note: give answers to four decimal places, for example: 99.1234.
+    diameter15 = 15 # cm
+    waferCost15 = 12
+    dies15 = 84
+    defectRate15 = .02 # cm^-2
+
+    diameter20 = 20 # cm
+    waferCost20 = 15
+    dies20 = 100
+    defectRate20 = .031 # cm^-2
+
+    waferYield15 = getWaferYield(diameter15, dies15, defectRate15)
+    waferYield20 = getWaferYield(diameter20, dies20, defectRate20)
+
+    dies15New = dies15 * 1.1
+    dies20New = dies20 * 1.1
+
+    defectRate15New = defectRate15 * 1.15
+    defectRate20New = defectRate20 * 1.15
+
+    def answer28():
+        print(waferYield15)
+
+    def answer29():
+        print(waferYield20)
+
+    def answer30():
+        dieCost15 = getDieCost(waferCost15, dies15, waferYield15)
+        print(dieCost15)
+
+    def answer31():
+        dieCost20 = getDieCost(waferCost20, dies20, waferYield20)
+        print(dieCost20)
+
+    def answer32():
+        dieArea15New = getDieArea(diameter15, dies15New)
+        print(dieArea15New)
+
+    def answer33():
+        dieArea20New = getDieArea(diameter20, dies20New)
+        print(dieArea20New)
+
+    def answer34():
+        waferYield15New = getWaferYield(diameter15, dies15New, defectRate15New)
+        print(waferYield15New)
+
+    def answer35():
+        waferYield20New = getWaferYield(diameter20, dies20New, defectRate20New)
+        print(waferYield20New)
+
+    def answer36():
+        defectRate92 = getDefectRate(.92)
+        print(defectRate92)
+
+    def answer37():
+        defectRate95 = getDefectRate(.95)
+        print(defectRate95)
+
+    # answer28()
+    # answer29()
+    # answer30()
+    # answer31()
+    # answer32()
+    # answer33()
+    # answer34()
+    # answer35()
+    # answer36()
+    answer37()
+
+def final():
+    # answer_02()
+    # answer_03()
+    # answer_04_p1()
+    # answer_04_p2()
+    # answer_04_p3()
+    # answer_04_p4()
+    # answer_04_p5()
+    # answer_04_p6()
+    # answer_06()
+    # answers_10_14()
+    # answer_15()
+    # answer_17()
+    # answer_25_p1()
+    # answer_25_p2()
+    # answer_25_p3()
+    # answer_25_p4()
+    # answer_25_p5()
+    # answer_25_p6()
+    answers_28_37()
+
+################################################################################
 
 if __name__ == '__main__':
-    midterm()
+    # midterm()
+    final()
