5afadb7e262daab7dee6d33ca0107cb8ec36c87e
Vu <vuphan314@users.noreply.github.com>
2022-08-01 07:43:03 -0700

    misc.sh

--- misc.sh
+++ misc.sh
@@ -2,48 +2,35 @@
 
 ################################################################################
 
-LS=l
-
-################################################################################
-
-## receives $@
-## sets $action
-function set_action {
-  if [[ -z $@ ]]; then
-    action="git status"
-  else
-    action=$@
-  fi
-}
-
-################################################################################
-
 function execute_ls {
-  tree ~ -L 2 --dirsfirst -I 'bin|code|snap|Zotero'
+  if [[ -d ~/Pictures/Screenshots ]]; then
+    rmdir ~/Pictures/Screenshots
+  fi
+  tree ~ --dirsfirst -L 2 -I 'bin|code|Documents|Music|Public|snap|Templates|Videos|Zotero'
 }
 
 ################################################################################
 
-## receives $action
 function execute_git {
   for d in $(find ~/code ~/Documents -mindepth 1 -maxdepth 1 | sort); do
     echo =======================================================================
     echo $d
     cd $d
-    $action
+    if [[ -z $@ ]]; then
+      git status
+    else
+      $@
+    fi
   done
 }
 
 ################################################################################
 
-## receives $@
-## sets $action
 function walk {
-  set_action $@
-  if [[ $action == $LS ]]; then
+  if [[ $@ == l ]]; then
     execute_ls
   else
-    execute_git
+    execute_git $@
   fi
 }
 
